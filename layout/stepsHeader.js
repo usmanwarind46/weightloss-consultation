e1cebd80216d1e19ec6d5bcec0571e6b08f9e54a
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-05-18 17:44:13 +0500

    changes

--- layout/stepsHeader.js
+++ layout/stepsHeader.js
@@ -129,13 +129,20 @@ const StepsHeader = ({ isOpen, toggleSidebar, percentage }) => {
   };
 
   const validPathDashboard =
-    pathname === "/dashboard/" || pathname === "/dashboard";
-  pathname === "/profile/" || pathname === "/profile";
-  pathname === "/orders/" || pathname === "/orders";
-  pathname === "/address/" || pathname === "/address";
-  pathname === "/change-password/" || pathname === "/change-password";
-  pathname === "/order-detail/" || pathname === "/order-detail";
-  pathname === "/weight-loss-journey/" || pathname === "/weight-loss-journey";
+    pathname === "/dashboard/" ||
+    pathname === "/dashboard" ||
+    pathname === "/profile/" ||
+    pathname === "/profile" ||
+    pathname === "/orders/" ||
+    pathname === "/orders" ||
+    pathname === "/address/" ||
+    pathname === "/address" ||
+    pathname === "/change-password/" ||
+    pathname === "/change-password" ||
+    pathname === "/order-detail/" ||
+    pathname === "/order-detail" ||
+    pathname === "/weight-loss-journey/" ||
+    pathname === "/weight-loss-journey";
 
   const loginMutation = useMutation(Login, {
     onSuccess: (data) => {
