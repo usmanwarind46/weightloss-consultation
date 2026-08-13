be68f7422024f66eaeeea1946b157f87cc33bb1f
usmanwarind46 <usmanwarind.vmi@gmail.com>
2026-08-13 15:33:29 +0500

    change abandon cart condition in route guard

--- utils/RouteGuard.js
+++ utils/RouteGuard.js
@@ -13,8 +13,11 @@ export default function RouteGuard({ children }) {
     const path = router.pathname;
     const isPublic = publicRoutes.includes(path);
     const isLogin = path === loginRoute;
+    const isAbandonedCart = router.asPath.includes("abandoned-cart");
 
-    if (!isPublic && !token) {
+    if (isLogin && token && isAbandonedCart) {
+      setLoading(false); // login.js ko render hone do, woh khud handle karega
+    } else if (!isPublic && !token) {
       router.push("/login/");
     } else if (isLogin && token && review) {
       router.push("/review");
